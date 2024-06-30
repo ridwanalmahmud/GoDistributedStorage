cc905930843536781d5732e3c2e6a7506aa2c524
Ridwan Al Mahmud <162663392+Ridwan-Al-Mahmud@users.noreply.github.com>
2024-06-30 17:56:19 +0000

    developing a tcp connectionwith remote and local nodes via TCPTransport

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	// "fmt"
+	"log"
+	"github.com/Ridwan-Al-Mahmud/GoDistributedStorage/p2p"
+)
+
+func main() {
+	tr := p2p.NewTCPTransport(":3000")
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+	select{}
+}
