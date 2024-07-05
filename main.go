524e4743f09dd8959d0ef4d1e124ef024dbc2212
Ridwan Al Mahmud <162663392+Ridwan-Al-Mahmud@users.noreply.github.com>
2024-07-05 07:07:40 +0000

    fixing delay issues

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
     "log"
 	"fmt"
 	"bytes"
-	"io/ioutil"
+	"io"
     "time"
 	"github.com/Ridwan-Al-Mahmud/GoDistributedStorage/p2p"
 )
@@ -30,31 +30,42 @@ func makeServer(listenAddr string, nodes ...string) *FileServer {
 
 func main() {
 	s1 := makeServer(":3000", "")
-	s2 := makeServer(":4000", ":3000")
+	s2 := makeServer(":4000", "")
 	s3 := makeServer(":5000", ":3000", ":4000")
+	
 	go func() {
-		go func(){ log.Fatal(s1.Start()) }()
-		time.Sleep(time.Millisecond * 500)
-		go func(){ log.Fatal(s2.Start()) }()
+		log.Fatal(s1.Start())
 	}()
-	time.Sleep(2 * time.Second)
-	go s3.Start()
-	time.Sleep(2 * time.Second)
-	key := "my_cool_picture.jpg"
-	data := bytes.NewReader([]byte("my big data file here"))
-	s3.Store(key, data)
+	time.Sleep(time.Millisecond * 500)
+	go func() {
+		log.Fatal(s2.Start())
+	}()
+	time.Sleep(time.Millisecond * 500)
+	go func() {
+		log.Fatal(s3.Start())
+	}()
 
-	if err := s3.store.Delete(key); err != nil {
+	time.Sleep(2 * time.Second)
+	
+	key := "my_cool_picture.jpg"
+	data := bytes.NewReader([]byte("my big data file here!"))
+	if err := s3.Store(key, data); err != nil {
 		log.Fatal(err)
 	}
+
+	time.Sleep(2 * time.Second)
 	
-    r, err := s3.Get(key)
+	r, err := s3.Get(key)
 	if err != nil {
 		log.Fatal(err)
 	}
-	b, err := ioutil.ReadAll(r)
+	b, err := io.ReadAll(r)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(string(b))
+
+	if err := s3.store.Delete(key); err != nil {
+		log.Fatal(err)
+	}
 }
